346a9e4cc76a1fb0b195a5170df0297642c9a586
hongkilsoon <basicjava0002@gmail.com>
2025-12-29 18:03:59 +0900

    2025.12.29 member1 - Member1Controller 새로운 Method 기능 추가5

--- src/main/java/kr/or/ddit/member1/Member1Controller.java
+++ src/main/java/kr/or/ddit/member1/Member1Controller.java
@@ -36,4 +36,11 @@ public class Member1Controller {
 		return "member1/start";
 	}
 	
+	// member1 작업
+	@GetMapping("/start5")
+	public String member1Start5(Model model) {
+		model.addAttribute("param", "member1-start5-parameter");
+		return "member1/start";
+	}
+	
 }
